4c53b67744282983ebc1c0b35d89bb034fa92eea
fullStackDeveloperToBe <gowdadushyanth27@gmail.com>
2022-03-10 11:00:03 +0530

    first commit

---
+++ src/main/java/com/pinaka/airlines/PinakaAirlinesApplication.java
@@ -0,0 +1,13 @@
+package com.pinaka.airlines;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinakaAirlinesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinakaAirlinesApplication.class, args);
+	}
+
+}
